00ec81b89de3c9c110e6251f2e3567a14ef555c4
Aditya Kumar Tiwari <145536614+adityaa1205@users.noreply.github.com>
2025-08-24 21:19:15 +0530

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -2,3 +2,4 @@
 echo "Downloading dlib landmark model..."
 wget -O shape_predictor_68_face_landmarks.dat.bz2 http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+
